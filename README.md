d3c54f64a222af39667d435a3c84009eecfa748d
Mike Winter <mike.winter@usask.ca>
2019-05-02 13:45:04 -0600

    Add docker registry install.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# radiam-deploy
-Ansible scripts to deploy 
+## Deployment infrastructure for radiam project
+
+# Deploy docker registry
+
+*Purpose*: Store static images versions of external projects and latest stable builds of the radiam project.
+
+*Howto*: ansible-playbook -i <inventory file (.ini)> registry-hosts.yml
+
+*Notes*: The radiam-api and radiam-gui images are pushed to this repository externally. Currently, this is done by the usask jenkins build server
+
+
---
+++ roles/registry/files/populate-registry.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# This will populate a local registry with defined versions of the radiam external images.
+
+ES_VERSION=6.4.2
+POSTGRES_VERSION=11.2-alpine
+MEMCACHED_VERSION=1.5-alpine
+NGINX_VERSION=1.15
+
+REGISTRY_HOST=build.radiam.ca
+REGISTRY_ENDPOINT=$REGISTRY_HOST
+
+echo -n "Installing elasticsearch image"
+docker pull docker.elastic.co/elasticsearch/elasticsearch:$ES_VERSION && \
+    docker tag docker.elastic.co/elasticsearch/elasticsearch:$ES_VERSION $REGISTRY_ENDPOINT/elastic && \
+    docker push $REGISTRY_ENDPOINT/elastic
+
+echo -n "Installing postgres image"
+docker pull postgres:$POSTGRES_VERSION && \
+    docker tag postgres:$POSTGRES_VERSION $REGISTRY_ENDPOINT/postgres && \
+    docker push $REGISTRY_ENDPOINT/postgres
+
+echo -n "Installing memcached image"
+docker pull memcached:$MEMCACHED_VERSION && \
+    docker tag memcached:$MEMCACHED_VERSION $REGISTRY_ENDPOINT/memcached && \
+    docker push $REGISTRY_ENDPOINT/memcached
+
+echo -n "Installing nginx image"
+docker pull nginx:$NGINX_VERSION && \
+    docker tag nginx:$NGINX_VERSION $REGISTRY_ENDPOINT/nginx && \
+    docker push $REGISTRY_ENDPOINT/nginx
