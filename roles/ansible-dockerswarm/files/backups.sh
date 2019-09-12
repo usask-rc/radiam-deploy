f1a0a2c07784d54ce728a56fba17ac7406f280bd
Todd Trann <todd.trann@usask.ca>
2019-09-12 14:13:40 -0600

    Make backups silent

--- roles/ansible-dockerswarm/files/backups.sh
+++ roles/ansible-dockerswarm/files/backups.sh
@@ -32,10 +32,10 @@ find /var/backups/docker/db/ -type f -mtime +10 -exec rm -f {} \;
 #################
 
 # Only need to run this once, but safe to run it every time to ensure backup snapshot index is created
-docker exec $ESCONT curl -XPUT "http://localhost:9200/_snapshot/esbackup" -H "Content-Type: application/json;charset=UTF-8" -d '{"type": "fs","settings": {"location": "/backups"}}'
+docker exec $ESCONT curl -s -XPUT "http://localhost:9200/_snapshot/esbackup" -H "Content-Type: application/json;charset=UTF-8" -d '{"type": "fs","settings": {"location": "/backups"}}' > /dev/null
 
 # Delete old snapshot so that the new snapshot is not incremental
-docker exec $ESCONT curl -XDELETE "localhost:9200/_snapshot/esbackup/snapshot_1"
+docker exec $ESCONT curl -s -XDELETE "localhost:9200/_snapshot/esbackup/snapshot_1" > /dev/null
 
 # Tell ES to perform the snapshot, do not wait for completion
-docker exec $ESCONT curl -XPUT "localhost:9200/_snapshot/esbackup/snapshot_1" -H "Content-Type: application/json;charset=UTF-8" -d '{"ignore_unavailable": true }'
+docker exec $ESCONT curl -s -XPUT "localhost:9200/_snapshot/esbackup/snapshot_1" -H "Content-Type: application/json;charset=UTF-8" -d '{"ignore_unavailable": true }' > /dev/null
